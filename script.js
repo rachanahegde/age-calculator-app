98b36a99549610f1bf2be81e612691680329e696
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2023-06-28 20:15:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-// Change color of arrow when it's clicked on
-
-// function changeBackground() {
-//   var arrowDiv = document.getElementById('down-arrow'); // Retrieve div element
-//   arrowDiv.style.backgroundColor = 'hsl(0, 0%, 8%)'; // Set background color to black
-// }
-
+// Change background color of arrow when mouse button presses down on div
 var arrowDiv = document.getElementById('down-arrow');
 
-// Change background color when mouse button presses down on div
 arrowDiv.addEventListener('mousedown', function() {
     arrowDiv.style.backgroundColor = 'hsl(0, 0%, 8%)';
-  });
+});
   
-  arrowDiv.addEventListener('mouseup', function() {
+arrowDiv.addEventListener('mouseup', function() {
     arrowDiv.style.backgroundColor = 'hsl(259, 100%, 65%)';
-  });
+});
+
+// Display age in years, months, and days after submitting a valid date through the form
+function displayAge() {
+    
+}
+
+// Receive validation errors if:
+    // Any field is empty when the form is submitted
+    // The day number is not between 1-31
+    // The month number is not between 1-12
+    // The year is in the future
+    // The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+
+// Bonus: See the age numbers animate to their final number when the form is submitted
