bfa863b5a1ff123cda60ee637818292e408d2e47
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2023-07-18 16:37:19 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+"use strict";
+
 const arrowDiv = document.getElementById("down-arrow");
-const resultDays = document.getElementById("result-days").textContent;
-const resultMonths = document.getElementById("result-months").textContent;
-const resultYears = document.getElementById("result-years").textContent;
+const resultDays = document.getElementById("result-days");
+const resultMonths = document.getElementById("result-months");
+const resultYears = document.getElementById("result-years");
 
 arrowDiv.addEventListener("click", function () {
   //   arrowDiv.style.backgroundColor = "hsl(0, 0%, 8%)";
@@ -27,96 +29,101 @@ function removeErrorMessage(field) {
 
 // Display age in years, months, and days after submitting a valid date through the form
 function displayAge() {
-  const day = document.getElementById("day").value;
-  const month = document.getElementById("month").value;
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
   const year = document.getElementById("year").value;
-  const dateValue = `${year}-${month}-${day}`;
-  const newDate = new Date(dateValue); // Create a new Date object from the input value
 
   const currentYear = new Date().getFullYear();
   const currentMonth = new Date().getMonth() + 1; // Get the current month (returns a value from 0 to 11)
   const currentDay = new Date().getDate();
 
-  if (!day || day < 1 || day > 31) {
+  // Receive validation errors if any field is empty when the form is submitted
+  if (!day || !month || !year) {
     if (!day) {
       displayRequiredField("day", "This field is required");
-    } else if (day > 31 || day < 1) {
-      displayRequiredField("day", "Must be a valid day");
+    } else {
+      removeErrorMessage("day");
     }
-  } else {
-    removeErrorMessage("day");
-  }
 
-  if (!month || month > 12 || month < 1) {
     if (!month) {
-      displayRequiredField("day", "This field is required");
-    } else if (month > 12 || month < 1) {
-      displayRequiredField("month", "Must be a valid month");
+      displayRequiredField("month", "This field is required");
+    } else {
+      removeErrorMessage("month");
     }
-  } else {
-    removeErrorMessage("day");
-  }
 
-  if (!year || year > 2023) {
     if (!year) {
       displayRequiredField("year", "This field is required");
-    } else if (year > 2023) {
-      displayRequiredField("year", "Must be in the past");
+    } else {
+      removeErrorMessage("year");
     }
-  } else {
-    removeErrorMessage("day");
-  }
-
-  if (day && month && year && isNaN(newDate.getTime())) {
-    displayRequiredField("day", "Must be a valid date");
-    displayRequiredField("month", "");
-    displayRequiredField("year", "");
   } else {
     removeErrorMessage("day");
     removeErrorMessage("month");
     removeErrorMessage("year");
   }
 
-  // Receive validation errors if any field is empty when the form is submitted
-  //   if (!day || !month || !year) {
-  //     console.log("This is getting triggered");
-  //     if (!day) {
-  //       displayRequiredField("day", "This field is required");
-  //     } else {
-  //       removeErrorMessage("day");
-  //     }
+  // Display error if day, month or year input fields aren't within specified bounds
+  if (day !== "") {
+    if (day < 1 || day > 31) {
+      displayRequiredField("day", "Must be a valid day");
+    }
+  }
 
-  //     if (!month) {
-  //       displayRequiredField("month", "This field is required");
-  //     } else {
-  //       removeErrorMessage("month");
-  //     }
+  if (month !== "") {
+    if (month < 1 || month > 12) {
+      displayRequiredField("month", "Must be a valid month");
+    }
+  }
 
-  //     if (!year) {
-  //       displayRequiredField("year", "This field is required");
-  //     } else {
-  //       removeErrorMessage("year");
-  //     }
-  //   } else {
-  //     removeErrorMessage("day");
-  //     removeErrorMessage("month");
-  //     removeErrorMessage("year");
-  //   }
+  if (year !== "") {
+    if (year.length > 4 || year.length < 4 || year > 2023) {
+      displayRequiredField("year", "Must be in the past");
+    }
+  }
 
-  //   if (isNaN(newDate.getTime())) {
-  //     displayRequiredField("day", "Must be a valid date");
-  //   } else {
-  //     removeErrorMessage("day");
-  //   }
+  // Check if date is valid
+  if (day !== "" && month !== "" && year !== "") {
+    if (0 < day < 32 && 0 < month < 13 && year <= 2023) {
+      day = Number(day) < 10 ? "0" + day : day;
+      month = Number(month) < 10 ? "0" + month : month;
+      const dateString = `${year}-${month}-${day}`;
+      const date = moment(dateString, "YYYY-MM-DD", true);
+      if (!date.isValid()) {
+        displayRequiredField("day", "Must be a valid date");
+        displayRequiredField("month", "");
+        displayRequiredField("year", "");
 
-  // Receive validation errors if:
-  // The day number is not between 1-31
-  // The month number is not between 1-12
-  // The year is in the future
+        // Display age
+      } else {
+        const birthDate = new Date(`${year}-${month}-${day}`); // Convert birthdate to Date object
+        const today = new Date(); // New Date object representing current date and time
 
-  // The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+        // Calculate age in years, months, and days
+        let ageYears = today.getFullYear() - birthDate.getFullYear();
+        let ageMonths = today.getMonth() - birthDate.getMonth();
+        let ageDays = today.getDate() - birthDate.getDate();
+
+        // Handle cases where birthday hasn't occured in current year or current month
+        if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0)) {
+          ageYears--;
+          ageMonths += 12;
+          if (ageDays < 0) {
+            ageMonths--;
+            ageDays += new Date(
+              today.getFullYear(),
+              today.getMonth(),
+              0
+            ).getDate();
+          }
+        }
+
+        resultDays.textContent = ageDays;
+        resultMonths.textContent = ageMonths;
+        resultYears.textContent = ageYears;
+      }
+    }
+  }
 }
 
-// Check that the UI looks good with error messages displayed for phone + tablet screen sizes
-
-// Bonus: See the age numbers animate to their final number when the form is submitted
+// TODO Check that the UI looks good with error messages displayed for phone + tablet screen sizes
+// TODO Refactor JS code and rename functions - i.e. the displayRequiredField function should be renamed displayErrorMessage function
