628cbcc9578680066bd12be1d478aac59a600935
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2023-07-18 22:02:54 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,21 +6,17 @@ const resultMonths = document.getElementById("result-months");
 const resultYears = document.getElementById("result-years");
 
 arrowDiv.addEventListener("click", function () {
-  //   arrowDiv.style.backgroundColor = "hsl(0, 0%, 8%)";
   displayAge();
 });
 
-// arrowDiv.addEventListener("mouseup", function () {
-//   arrowDiv.style.backgroundColor = "hsl(259, 100%, 65%)";
-// });
-
-// Display required field error to user
-function displayRequiredField(field, errorMessage) {
+// Display error to user
+function displayErrorMessage(field, errorMessage) {
   document.getElementById(`${field}`).classList.add("empty-error");
   document.getElementById(`${field}-descrip`).style.color = "hsl(0, 65%, 67%)";
   document.getElementById(`${field}-error`).textContent = errorMessage;
 }
 
+// Remove error message
 function removeErrorMessage(field) {
   document.getElementById(`${field}`).classList.remove("empty-error");
   document.getElementById(`${field}-descrip`).style.color = "hsl(0, 1%, 44%)";
@@ -40,19 +36,19 @@ function displayAge() {
   // Receive validation errors if any field is empty when the form is submitted
   if (!day || !month || !year) {
     if (!day) {
-      displayRequiredField("day", "This field is required");
+      displayErrorMessage("day", "This field is required");
     } else {
       removeErrorMessage("day");
     }
 
     if (!month) {
-      displayRequiredField("month", "This field is required");
+      displayErrorMessage("month", "This field is required");
     } else {
       removeErrorMessage("month");
     }
 
     if (!year) {
-      displayRequiredField("year", "This field is required");
+      displayErrorMessage("year", "This field is required");
     } else {
       removeErrorMessage("year");
     }
@@ -65,19 +61,19 @@ function displayAge() {
   // Display error if day, month or year input fields aren't within specified bounds
   if (day !== "") {
     if (day < 1 || day > 31) {
-      displayRequiredField("day", "Must be a valid day");
+      displayErrorMessage("day", "Must be a valid day");
     }
   }
 
   if (month !== "") {
     if (month < 1 || month > 12) {
-      displayRequiredField("month", "Must be a valid month");
+      displayErrorMessage("month", "Must be a valid month");
     }
   }
 
   if (year !== "") {
     if (year.length > 4 || year.length < 4 || year > 2023) {
-      displayRequiredField("year", "Must be in the past");
+      displayErrorMessage("year", "Must be in the past");
     }
   }
 
@@ -89,9 +85,9 @@ function displayAge() {
       const dateString = `${year}-${month}-${day}`;
       const date = moment(dateString, "YYYY-MM-DD", true);
       if (!date.isValid()) {
-        displayRequiredField("day", "Must be a valid date");
-        displayRequiredField("month", "");
-        displayRequiredField("year", "");
+        displayErrorMessage("day", "Must be a valid date");
+        displayErrorMessage("month", "");
+        displayErrorMessage("year", "");
 
         // Display age
       } else {
@@ -107,6 +103,7 @@ function displayAge() {
         if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0)) {
           ageYears--;
           ageMonths += 12;
+          // Get number of days in the previous month and add to ageDays
           if (ageDays < 0) {
             ageMonths--;
             ageDays += new Date(
@@ -124,7 +121,3 @@ function displayAge() {
     }
   }
 }
-
-// TODO Finish reviewing the code explanations
-// TODO Check that the UI looks good with error messages displayed for phone + tablet screen sizes
-// TODO Refactor JS code and rename functions - i.e. the displayRequiredField function should be renamed displayErrorMessage function
